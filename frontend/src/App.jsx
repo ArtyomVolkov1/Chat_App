9cc5a2797058e6764a4906b40f50881c8206612f
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-19 19:50:57 +0300

    Added changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './Components/AppRouter';
+// import Navbar from './Components/Navbar';
+
+const App = () => (<BrowserRouter><AppRouter /></BrowserRouter>);
+export default App;
