61c8b17e8f9f167b5a5dcdd88410fec81904530c
Artyom Volkov <artemvolk2099@yandex.ru>
2023-07-09 16:41:01 +0300

    Added changes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
