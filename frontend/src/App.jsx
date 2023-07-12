52257eda366eb1a6aa279510b513ae820c7b817f
Artyom Volkov <artemvolk2099@yandex.ru>
2023-07-12 17:25:08 +0300

    Added changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,36 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './Components/AppRouter';
 import './App.css';
 import Navbars from './Components/Navbar';
-// import AuthContext from './contexts';
-// import Navbar from './Components/Navbar';
-// const AuthProvider = ({ children }) => {
-//   const [loggedIn, setLoggedIn] = useState(false);
-//   const logIn = () => setLoggedIn(true);
-//   const logOut = () => {
-//     localStorage.removeItem('userId');
-//     setLoggedIn(false);
-//   };
-//   const checkAuth = () => {
-//     const userId = JSON.parse(localStorage.getItem('userId'));
-//     if (userId.token) {
-//       setLoggedIn(true);
-//     }
-//   };
-//   return (
-//     <AuthContext.Provider
-//       // eslint-disable-next-line react/jsx-no-constructed-context-values
-//       value={{
-//         loggedIn,
-//         logIn,
-//         logOut,
-//         checkAuth,
-//       }}
-//     >{children}
-//     </AuthContext.Provider>
-//   );
-// };
+import ChatApiProvider from './contexts/ChatApiContext';
 
-const App = () => (
-  <BrowserRouter><Navbars /><AppRouter /></BrowserRouter>
+const App = ({ socket }) => (
+  <BrowserRouter><div className="d-flex flex-column h-100 "><Navbars /><ChatApiProvider socket={socket}><AppRouter /></ChatApiProvider></div></BrowserRouter>
 );
 export default App;
--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -4,10 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from 'react-bootstrap/esm/Button';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import SendMessageForm from './SendMessageForm';
 import { fetchData } from '../store/slices/dataSlice';
+
 import routes from '../routes';
+import { addMessage } from '../store/slices/messagesSlice';
+import Body from './MessageList';
 
 const getAuthHeader = () => {
   const userId = JSON.parse(localStorage.getItem('userId'));
@@ -18,48 +21,53 @@ const getAuthHeader = () => {
 };
 
 const Chat = () => {
-  const { channels } = useSelector((state) => state.data);
+  const {
+    channels, currentChannelId,
+  } = useSelector((state) => state.data);
+  // const messageBody = useSelector((state) => state.data.messages.body);
   const dispatch = useDispatch();
   useEffect(() => {
     const fetchingData = async () => {
-      const responce = await axios.get(routes.usersPath(), { headers: getAuthHeader() });
+      const responce = await axios.get(routes.data(), { headers: getAuthHeader() });
       localStorage.setItem('userData', JSON.stringify(responce.data));
       dispatch(fetchData(responce.data));
-      console.log(responce);
+      dispatch(addMessage(responce.data.messages));
     };
     fetchingData();
   }, [dispatch]);
   return (
-    <Container className="h-100 my-4 overflow-hidden rounded shadow">
-      <Row>
-        <Col className="col-auto flex-column border-end px-0 h-100 d-flex min-vh-100 bg-light">
-          <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-3 border-bottom">
-            <b>Каналы</b>
-            <Button variant="outline-primary" className="p-1"><b>+</b></Button>
+    <Row className="h-100 bg-white flex-md-row w-100">
+      <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+        <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-3 border-bottom">
+          <b>Каналы</b>
+          <Button variant="group-vertical" className="p-0"><b className="pe-2">+</b></Button>
+        </div>
+        <Nav variant="pills" as="ul" id="channels-box" className="flex-column nav-fill px-2 mb-3 overflow-auto h-100 d-block ">
+          {channels.map((channel) => (
+            <Nav.Item as="li" className="w-100" key={channel.id}>
+              <Button key={currentChannelId} type="button" className="w-100 rounded-0 text-start btn-secondary">
+                <span className="me-1">#
+                </span>
+                {channel.name}
+              </Button>
+            </Nav.Item>
+          ))}
+        </Nav>
+      </div>
+      <Col className="col p-0 h-100">
+        <div className="d-flex flex-column h-100">
+          <div className="bg-light mb-4 p-3 shadow-sm small">
+            <p className="m-0">
+              <b>#general</b>
+            </p>
           </div>
-          <Nav variant="pills" as="ul" id="channels-box" className="flex-column ">
-            {channels.map((channel) => (
-              <Nav.Item as="li" className="w-100" key={channel.id}>
-                <Button type="button" className="w-100 rounded-0 text-start btn-secondary">
-                  <span className="me-1">#
-                  </span>
-                  {channel.name}
-                </Button>
-              </Nav.Item>
-            ))}
-          </Nav>
-        </Col>
-        <Col className="p-0 h-100">
-          <div className="d-flex flex-column h-100">
-            <div className="bg-light mb-4 p-3 shadow-sm small">
-              <p className="m-0">
-                <b>#general</b>
-              </p>
-            </div>
+          <Body />
+          <div className="mt-auto px-5 py-3">
+            <SendMessageForm />
           </div>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
