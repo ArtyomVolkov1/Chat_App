85b0bab0675ca8dd7765e6e370fda321877d5683
Artyom Volkov <artemvolk2099@yandex.ru>
2023-08-27 18:53:53 +0300

    Added changes and fix bugs

--- frontend/src/Components/Body/MessageList.jsx
+++ frontend/src/Components/Body/MessageList.jsx
@@ -1,23 +1,37 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { getMessages } from '../../store/slices/messagesSlice';
-import { getCurrentChannelId } from '../../store/slices/channelsSlice';
 
-const MessageList = ({ username, body }) => (
-  <div className="text-break mb-2">
-    <div><b>{username}</b>{': '} {body}</div>
+const MessageList = ({ messages }) => messages.map(({ username, body, id }) => (
+  <div key={id} className="text-break mb-2">
+    <div>
+      <b>{username}</b>
+      {': '} {body}
+    </div>
+  </div>
+));
+
+const Body = ({ messages }) => (
+  <div className="chat-messages overflow-auto px-5">
+    <MessageList messages={messages} />
   </div>
 );
-
-const Body = () => {
-  const messages = useSelector(getMessages);
-  const currentChannelId = useSelector(getCurrentChannelId);
-  const messagesInChat = messages.filter(({ channelId }) => channelId === currentChannelId);
-  return (
-    <div className="chat-messages overflow-auto px-5">{messagesInChat.map(({ id, username, body }) => (
-      <MessageList key={id} username={username} body={body} />
-    ))}
-    </div>
-  );
-};
 export default Body;
+
+// const MessageList = ({ username, body }) => (
+//   <div className="text-break mb-2">
+//     <div><b>{username}</b>{': '} {body}</div>
+//   </div>
+// );
+
+// const Body = () => {
+//   const messages = useSelector(getMessages);
+//   const currentChannelId = useSelector(getCurrentChannelId);
+//   const messagesInChat = messages.filter(({ channelId }) => channelId === currentChannelId);
+//   return (
+//     <div className="chat-messages overflow-auto px-5">{messagesInChat.map(({ id, username, body }) => (
+//       <MessageList key={id} username={username} body={body} />
+//     ))}
+//     </div>
+//   );
+// };
+// export default Body;
--- frontend/src/Components/Body/Messages.jsx
+++ frontend/src/Components/Body/Messages.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Body from './MessageList';
 import SendMessageForm from './SendMessageForm';
 import Header from './Header';
+import { getCurrentChannelId } from '../../store/slices/channelsSlice';
+import { getMessages } from '../../store/slices/messagesSlice';
 
-const Messages = () => (
-  <div className="col p-0 h-100">
-    <div className="d-flex flex-column h-100">
-      <Header />
-      <Body />
-      <SendMessageForm />
+const Messages = () => {
+  const messages = useSelector(getMessages);
+  console.log(messages);
+  const currentChannelId = useSelector(getCurrentChannelId);
+  const channelMessages = messages.filter(
+    ({ channelId }) => channelId === currentChannelId,
+  );
+  return (
+    <div className="col p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <Header />
+        <Body messages={channelMessages} />
+        <SendMessageForm />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Messages;
