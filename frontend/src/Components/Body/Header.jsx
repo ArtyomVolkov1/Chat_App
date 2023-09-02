86200db3d92e9f54c37dd150355f4b2d9ba2826b
Artyom Volkov <artemvolk2099@yandex.ru>
2023-09-02 23:10:00 +0300

    Fix lint

--- frontend/src/Components/Body/Header.jsx
+++ frontend/src/Components/Body/Header.jsx
@@ -11,7 +11,8 @@ const Header = () => {
   return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
       <p className="m-0">
-        <b>#
+        <b>
+          #
           {' '}
           {currentChannelName?.name}
         </b>
