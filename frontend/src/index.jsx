cc43bec0df4db45b8298f33fa45bfb193cf6be8e
Artyom Volkov <artemvolk2099@yandex.ru>
2023-10-17 16:29:44 +0300

    Fix bugs

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { io } from 'socket.io-client';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import init from './init';
 
